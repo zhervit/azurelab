581577e16a34cf0947c860837f27f694f7fcda52
Victor Zherdev <victor_zherdev@epam.com>
2018-12-03 15:43:06 +0300

    task 04

---
+++ Lab_04/AzureWorkshop/AzureWorkshop/Models/FileEntity .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureWorkshop.Models
+{
+	public class FileEntity : TableEntity
+	{
+		public FileEntity() { }
+
+		public FileEntity(string blobId)
+			: base("Images", blobId) { }
+
+		public string Name { get; set; }
+		public string ClientAddress { get; set; }
+	}
+
+}
